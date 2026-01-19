35fbcd2118e47e4386f0664ae87d036bd52a0590
VerbosalSpy <lukaszplfajnyy@gmail.com>
2026-01-19 09:35:25 +0100

    Set up the basic framework for the app

--- projekt04/index.js
+++ projekt04/index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+import { readFileSync } from "node:fs";
 
+//const index_html = readFileSync("./public/index.html");
 const port = 8000;
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(morgan("dev"));
+
+function log_request(req, res, next) {
+    console.log(`Request taken! Method: ${req.method} Path: ${req.path}`);
+    next();
+}
+
+app.all("/", function(req, res) {
+    RedirectHandler.render("./index.html")
+})
+
+// app.get("/",function(req, res) {
+//     res.end(index_html)
+// })
+
+app.use(log_request);
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
