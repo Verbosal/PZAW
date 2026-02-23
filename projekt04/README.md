08e1ad7daa857cdd4bfa46b2bdb7a0b0ebfb297c
VerbosalSpy <lukaszplfajnyy@gmail.com>
2026-02-23 09:36:08 +0100

    Set up the backbone of the posting system

--- projekt04/README.md
+++ projekt04/README.md
@@ -2,9 +2,10 @@ WIP!!!
 
 # Simple silly forum
 ## You can:
--Make a post (markdown and embedded hopefully included) ONCE you have an account!!! (and are logged in ofc),
--See all posts using the POST method (get it?),
+-Make an account and log in,
+-Make a post (using the POST method),
+-See all posts,
 -Modify and delete your own posts
--Oh, and you can get an admin and modify other people's posts.
+-And you can be an admin and modify other people's posts.
 
 Just set up your desired administrator credentials in admin.json and run ```node index.js``` to start hosting the website @ localhost:8000.
--- projekt04/index.js
+++ projekt04/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import morgan from "morgan";
-import { readFileSync } from "node:fs";
+import bcrypt from "bcrypt";
+import session from "express-session";
 
 //const index_html = readFileSync("./public/index.html");
 const port = 8000;
@@ -8,22 +8,21 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(morgan("dev"));
 
-function log_request(req, res, next) {
-    console.log(`Request taken! Method: ${req.method} Path: ${req.path}`);
-    next();
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.all("/", function(req, res) {
-    RedirectHandler.render("./index.html")
-})
+app.post("/", async (req, res) => {
+  var params = req.body
+  //console.log(params);
 
-// app.get("/",function(req, res) {
-//     res.end(index_html)
-// })
+  // res.send({
+  //   'title': params.title,
+  //   'content': params.content
+  // });
 
-app.use(log_request);
+  res.sendFile("/", {root:"./public"});
+});
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
